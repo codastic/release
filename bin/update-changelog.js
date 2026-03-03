9787368cc324fd0febfc2e71f3279b8f8ba8ea52
Mario Volke <mv@codastic.com>
2026-03-03 15:33:56 +0100

    Lint fix

--- bin/update-changelog.js
+++ bin/update-changelog.js
@@ -1,33 +1,33 @@
 #!/usr/bin/env node
 
-const childProcess = require("child_process");
-const fs = require("fs");
-const path = require("path");
-const readline = require("readline");
-const minimist = require("minimist");
-const prompts = require("prompts");
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const minimist = require('minimist');
+const prompts = require('prompts');
 
 const argv = minimist(process.argv, {
   boolean: [
-    "dry-run",
-    "help",
-    "hide-reviewer",
-    "hide-implementer",
-    "all-commits",
+    'dry-run',
+    'help',
+    'hide-reviewer',
+    'hide-implementer',
+    'all-commits'
   ],
-  string: ["since"],
+  string: ['since'],
   default: {
-    "dry-run": false,
-    "hide-reviewer": false,
-    "hide-implementer": false,
-    "all-commits": false,
-  },
+    'dry-run': false,
+    'hide-reviewer': false,
+    'hide-implementer': false,
+    'all-commits': false
+  }
 });
 
 const { exec } = childProcess;
 const rootDirectory = argv._.pop();
 
-const changelogName = "CHANGELOG.md";
+const changelogName = 'CHANGELOG.md';
 const changelogPath = path.resolve(process.cwd(), rootDirectory, changelogName);
 
 if (argv.help) {
@@ -35,33 +35,32 @@ if (argv.help) {
     [
       `Adds merged pulled requests since last versioned release to ${changelogName}`,
       'Supports pull requests from Github and Bitbucket. Uses "git" and "npm".\n',
-      "Usage: update-changelog ROOT_DIR",
-      "Options:",
-      "  --dry-run (optional) Outputs changes instead of writing to CHANGELOG.md.",
+      'Usage: update-changelog ROOT_DIR',
+      'Options:',
+      '  --dry-run (optional) Outputs changes instead of writing to CHANGELOG.md.',
       '  --since (optional) Limit search for pull requests to the given ISO date (e.g. "2017-01-01").',
-      "  --hide-reviewer (optional) If set the reviewer will not be put into the CHANGELOG.md.",
-      "  --hide-implementer (optional) If set the implementer will not be put into the CHANGELOG.md.",
-      "  --interactive (optional) If set every addition to the changelog has to be confirmed manually.",
-      "  --link-commit (optional) If set the commit hash in the output will be linked.",
-      "  --all-commits (optional) If set all commits will be checked for merge messages and not only merge commits. Use this if you squash merge pull requests." +
-        ' Expects a template URL in the form of "http://example.com/:commit".',
-    ].join("\n"),
+      '  --hide-reviewer (optional) If set the reviewer will not be put into the CHANGELOG.md.',
+      '  --hide-implementer (optional) If set the implementer will not be put into the CHANGELOG.md.',
+      '  --interactive (optional) If set every addition to the changelog has to be confirmed manually.',
+      '  --link-commit (optional) If set the commit hash in the output will be linked.',
+      '  --all-commits (optional) If set all commits will be checked for merge messages. Use this if you squash merge pull requests.'
+        + ' Expects a template URL in the form of "http://example.com/:commit".'
+    ].join('\n'),
   );
   process.exit(0);
 }
 
 function getPullRequests(callback) {
   // Get latest 500 pull requests
-  let cmd =
-    "git --no-pager log " +
-    "--grep 'Merged in .* (pull request #.*)' " + // Butbucket format
-    "--grep 'Merge pull request #.*' " + // Github format
-    "--grep 'Merged PR .*: .*' " + // VisualStudio Team Services format
-    "--pretty='format:__pr-start__%n%h%n%an%n%s%n%b%n__pr-end__' " +
-    "-500";
+  let cmd = 'git --no-pager log '
+    + "--grep 'Merged in .* (pull request #.*)' " // Butbucket format
+    + "--grep 'Merge pull request #.*' " // Github format
+    + "--grep 'Merged PR .*: .*' " // VisualStudio Team Services format
+    + "--pretty='format:__pr-start__%n%h%n%an%n%s%n%b%n__pr-end__' "
+    + '-500';
 
-  if (!argv["all-commits"]) {
-    cmd += " --merged";
+  if (!argv['all-commits']) {
+    cmd += ' --merges';
   }
 
   if (argv.since) {
@@ -75,23 +74,23 @@ function getPullRequests(callback) {
     const pullRequests = [];
     let group;
 
-    stdout.split("\n").forEach((line) => {
+    stdout.split('\n').forEach((line) => {
       if (!line) {
         // Filter empty lines
         return;
       }
 
-      if (line === "__pr-start__") {
+      if (line === '__pr-start__') {
         group = [];
-      } else if (line === "__pr-end__") {
+      } else if (line === '__pr-end__') {
         const commit = group.shift();
         const reviewer = group.shift();
         const subject = group.shift();
         const approvedBy = /^Approved-by:\s([^<]+)\s<([^>]+)>$/;
         let message = group
-          .filter((l) => !approvedBy.test(l))
+          .filter(l => !approvedBy.test(l))
           .join()
-          .replace(/\n|\r/g, "")
+          .replace(/\n|\r/g, '')
           .trim();
 
         // Visual Studio Team Services do not guarantee to provide the PR title within the body
@@ -103,7 +102,7 @@ function getPullRequests(callback) {
         pullRequests.push({
           commit,
           reviewer,
-          message,
+          message
         });
       } else {
         group.push(line);
@@ -119,25 +118,25 @@ function findCommitsInChangelog(callback) {
   const commits = [];
   const input = fs.createReadStream(changelogPath);
   const lineReader = readline.createInterface({
-    input,
+    input
   });
 
-  input.on("error", () => {
+  input.on('error', () => {
     console.error(
-      "Warning: %s does not exist, yet. Attempt to create file now.",
+      'Warning: %s does not exist, yet. Attempt to create file now.',
       changelogName,
     );
     callback([]);
   });
 
-  lineReader.on("line", (line) => {
+  lineReader.on('line', (line) => {
     const matches = matcher.exec(line);
     if (matches) {
       commits.push(matches[1]);
     }
   });
 
-  lineReader.on("close", () => {
+  lineReader.on('close', () => {
     callback(commits);
   });
 }
@@ -149,7 +148,7 @@ function matchAbbreviatedCommit(abbrCommit, commits) {
     }
   }
 
-  return commits.some((commit) => commit.startsWith(abbrCommit));
+  return commits.some(commit => commit.startsWith(abbrCommit));
 }
 
 function filterPullRequests(pullRequests, commits) {
@@ -182,12 +181,12 @@ function resolveImplementers(pullRequests, callback) {
       const pullRequest = pullRequests[pos];
 
       // eslint-disable-next-line no-param-reassign
-      pullRequest.implementer = stdout.replace(/\n|\r/g, "").trim();
+      pullRequest.implementer = stdout.replace(/\n|\r/g, '').trim();
 
       // In Bitbucket and Visual Studio Team Services it is not possible to find the reviewer within git data
       if (
-        argv["hide-reviewer"] ||
-        pullRequest.implementer === pullRequest.reviewer
+        argv['hide-reviewer']
+        || pullRequest.implementer === pullRequest.reviewer
       ) {
         pullRequest.reviewer = null;
       }
@@ -200,53 +199,54 @@ function resolveImplementers(pullRequests, callback) {
   next(0);
 }
 
-function stringifyPullRequest({ message, implementer, reviewer, commit }) {
-  const commitString = argv["link-commit"]
-    ? `[${commit}](${argv["link-commit"].replace(":commit", commit)})`
+function stringifyPullRequest({
+  message, implementer, reviewer, commit
+}) {
+  const commitString = argv['link-commit']
+    ? `[${commit}](${argv['link-commit'].replace(':commit', commit)})`
     : commit;
 
   const addInfo = [];
-  if (!argv["hide-implementer"]) {
+  if (!argv['hide-implementer']) {
     addInfo.push(`i: ${implementer}`);
   }
-  if (!argv["hide-reviewer"] && reviewer) {
+  if (!argv['hide-reviewer'] && reviewer) {
     addInfo.push(`r: ${reviewer}`);
   }
   addInfo.push(`c: ${commitString}`);
 
-  return `- ${message} (${addInfo.join(", ")})\n`;
+  return `- ${message} (${addInfo.join(', ')})\n`;
 }
 
 function filterPullRequestsInteractively(pullRequests) {
   return pullRequests.reduce(
-    (promise, pullRequest) =>
-      promise.then(async (filteredPullRequests) => {
-        const response = await prompts({
-          type: "confirm",
-          name: "value",
-          initial: true,
-          message: stringifyPullRequest(pullRequest),
-        });
+    (promise, pullRequest) => promise.then(async (filteredPullRequests) => {
+      const response = await prompts({
+        type: 'confirm',
+        name: 'value',
+        initial: true,
+        message: stringifyPullRequest(pullRequest)
+      });
 
-        if (!response.value) {
-          return filteredPullRequests;
-        }
+      if (!response.value) {
+        return filteredPullRequests;
+      }
 
-        return [...filteredPullRequests, pullRequest];
-      }),
+      return [...filteredPullRequests, pullRequest];
+    }),
     Promise.resolve([]),
   );
 }
 
 function prependToChangelog(str) {
-  let data = Buffer.from("");
+  let data = Buffer.from('');
   try {
     data = fs.readFileSync(changelogPath); // Read existing contents into data
   } catch (error) {
     // Ignore when changelog does not exist yet.
   }
 
-  const fd = fs.openSync(changelogPath, "w+");
+  const fd = fs.openSync(changelogPath, 'w+');
   const buffer = Buffer.from(str);
   fs.writeSync(fd, buffer, 0, buffer.length); // Write new data
   fs.writeSync(fd, data, 0, data.length); // Append old data
@@ -263,15 +263,15 @@ getPullRequests((pullRequests) => {
         : Promise.resolve(newPullRequests)
       ).then((finalPullRequests) => {
         if (finalPullRequests.length === 0) {
-          console.error("Error: There are no new changes!");
+          console.error('Error: There are no new changes!');
           process.exit(1);
         }
 
         const changeString = finalPullRequests
           .map(stringifyPullRequest)
-          .join("");
+          .join('');
 
-        if (argv["dry-run"]) {
+        if (argv['dry-run']) {
           console.log(changeString);
         } else {
           prependToChangelog(changeString);
@@ -279,7 +279,7 @@ getPullRequests((pullRequests) => {
       });
     };
 
-    if (argv["hide-implementer"]) {
+    if (argv['hide-implementer']) {
       processPullRequest();
     } else {
       resolveImplementers(newPullRequests, processPullRequest);
