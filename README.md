ff3d781a332fa6cf11705f851c3c7f3aaabb3536
Mario Volke <mv@codastic.com>
2026-03-03 15:26:39 +0100

    Add support for squash commits in update changelog

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # @codastic/release
+
 A collection of release and changelog management script you can easily use via NPM.
 
 ## Installation
@@ -10,7 +11,7 @@ $ npm install @codastic/release
 ## Features
 
 - Creates a `CHANGELOG.md` containing all commits starting with `Merge pull request…`.
-- Bumps the package version and adds version headlines to the  `CHANGELOG.md`.
+- Bumps the package version and adds version headlines to the `CHANGELOG.md`.
 - Creates a GitHub release.
 
 ## Quick Start
@@ -18,7 +19,7 @@ $ npm install @codastic/release
 Running `npm install @codastic/release` adds the following scripts to you local `node_modules/.bin` folder:
 
 - **update-changelog**: Adds merged pulled requests since last versioned release to `CHANGELOG.md`,
-Supports pull requests from Github and Bitbucket.
+  Supports pull requests from Github and Bitbucket.
 
 - **release**: Bumps a new versioned release of the current state into `CHANGELOG.md` and `package.json`.
 
@@ -51,15 +52,19 @@ $ update-changelog ROOT_DIR [OPTIONS]
 
 - `--help`: Outputs help.
 
-- `--dry-run`: *(optional)* Outputs changes instead of writing to `CHANGELOG.md`.
+- `--dry-run`: _(optional)_ Outputs changes instead of writing to `CHANGELOG.md`.
 
-- `--since`: *(optional)* Limit search for pull requests to the given ISO date (e.g. `--since='2017-01-01'`).
+- `--since`: _(optional)_ Limit search for pull requests to the given ISO date (e.g. `--since='2017-01-01'`).
 
-- `--hide-reviewer`: *(optional)* If set the reviewer will not be put into the `CHANGELOG.md`.
+- `--hide-reviewer`: _(optional)_ If set the reviewer will not be put into the `CHANGELOG.md`.
 
-- `--interactive`: *(optional)* If set every addition to the changelog has to be confirmed manually.
+- `--hide-implementer` _(optional)_ If set the implementer will not be put into the `CHANGELOG.md`.
 
-- `--link-commit`: *optional* If set the commit hash in the output will be linked. Expects a template URL in the form of "http://example.com/:commit".
+- `--interactive`: _(optional)_ If set every addition to the changelog has to be confirmed manually.
+
+- `--link-commit`: _optional_ If set the commit hash in the output will be linked. Expects a template URL in the form of "http://example.com/:commit".
+
+- `--all-commits`: (optional) If set all commits will be checked for merge messages and not only merge commits. Use this if you squash merge pull requests.
 
 ## release
 
@@ -71,11 +76,11 @@ $ release NPM_PACKAGE_DIR [<newversion> | major | minor | patch | prerelease] [o
 
 - `--help`: Outputs help.
 
-- `--push-build`: *(optional)* Push the build in this ignored folder to the version branch.
+- `--push-build`: _(optional)_ Push the build in this ignored folder to the version branch.
 
-- `--target-branch`: *(optional)* (default: MAJOR.x) The branch where the release will be pushed to (e.g. `--target-branch='master'`).
+- `--target-branch`: _(optional)_ (default: MAJOR.x) The branch where the release will be pushed to (e.g. `--target-branch='master'`).
 
-- `--build-command`: *(optional)* Run the build to be able to include the new version from `package.json` (e.g `--build-command='npm run build' ./"`). Also adds and commits the build files if they are not ignored.
+- `--build-command`: _(optional)_ Run the build to be able to include the new version from `package.json` (e.g `--build-command='npm run build' ./"`). Also adds and commits the build files if they are not ignored.
 
 ## release-github
 
@@ -94,22 +99,26 @@ If you run this script the first time, it asks for a GitHub token, which will be
 ## Requirements
 
 - Node 6.10 or higher is required.
+
 ```bash
 node --version  # 6.10 or higher
 ```
 
 - Git version 2.7 or higher is required for github release script.
+
 ```bash
 git --version  # 2.7 or higher
 ```
 
 - The repository needs to be checked out via SSH. [Need help?](https://help.github.com/articles/changing-a-remote-s-url/#switching-remote-urls-from-https-to-ssh)
+
 ```bash
 git remote set-url origin git@github.com:SevenOneMedia/adtec-core.git
 ```
 
 - The release script uses the GitHub API and therefore requires an [accesstoken](https://github.com/settings/tokens/new).
-The script will prompt automatically for that token once and stores it in `.git-release`. Make sure the following permissions are granted when creating a new token on GitHub:
- ```markdown
- [x] repo       Full control of private repositories
- ```
+  The script will prompt automatically for that token once and stores it in `.git-release`. Make sure the following permissions are granted when creating a new token on GitHub:
+
+```markdown
+[x] repo Full control of private repositories
+```
--- bin/update-changelog.js
+++ bin/update-changelog.js
@@ -1,52 +1,68 @@
 #!/usr/bin/env node
 
-const childProcess = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const minimist = require('minimist');
-const prompts = require('prompts');
+const childProcess = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const minimist = require("minimist");
+const prompts = require("prompts");
 
 const argv = minimist(process.argv, {
-  boolean: ['dry-run', 'help', 'hide-reviewer'],
-  string: ['since'],
+  boolean: [
+    "dry-run",
+    "help",
+    "hide-reviewer",
+    "hide-implementer",
+    "all-commits",
+  ],
+  string: ["since"],
   default: {
-    'dry-run': false,
-    'hide-reviewer': false
-  }
+    "dry-run": false,
+    "hide-reviewer": false,
+    "hide-implementer": false,
+    "all-commits": false,
+  },
 });
 
 const { exec } = childProcess;
 const rootDirectory = argv._.pop();
 
-const changelogName = 'CHANGELOG.md';
+const changelogName = "CHANGELOG.md";
 const changelogPath = path.resolve(process.cwd(), rootDirectory, changelogName);
 
 if (argv.help) {
-  console.log([
-    `Adds merged pulled requests since last versioned release to ${changelogName}`,
-    'Supports pull requests from Github and Bitbucket. Uses "git" and "npm".\n',
-    'Usage: update-changelog ROOT_DIR',
-    'Options:',
-    '  --dry-run (optional) Outputs changes instead of writing to CHANGELOG.md.',
-    '  --since (optional) Limit search for pull requests to the given ISO date (e.g. "2017-01-01").',
-    '  --hide-reviewer (optional) If set the reviewer will not be put into the CHANGELOG.md.',
-    '  --interactive (optional) If set every addition to the changelog has to be confirmed manually.',
-    '  --link-commit (optional) If set the commit hash in the output will be linked.'
-      + ' Expects a template URL in the form of "http://example.com/:commit".'
-  ].join('\n'));
+  console.log(
+    [
+      `Adds merged pulled requests since last versioned release to ${changelogName}`,
+      'Supports pull requests from Github and Bitbucket. Uses "git" and "npm".\n',
+      "Usage: update-changelog ROOT_DIR",
+      "Options:",
+      "  --dry-run (optional) Outputs changes instead of writing to CHANGELOG.md.",
+      '  --since (optional) Limit search for pull requests to the given ISO date (e.g. "2017-01-01").',
+      "  --hide-reviewer (optional) If set the reviewer will not be put into the CHANGELOG.md.",
+      "  --hide-implementer (optional) If set the implementer will not be put into the CHANGELOG.md.",
+      "  --interactive (optional) If set every addition to the changelog has to be confirmed manually.",
+      "  --link-commit (optional) If set the commit hash in the output will be linked.",
+      "  --all-commits (optional) If set all commits will be checked for merge messages and not only merge commits. Use this if you squash merge pull requests." +
+        ' Expects a template URL in the form of "http://example.com/:commit".',
+    ].join("\n"),
+  );
   process.exit(0);
 }
 
 function getPullRequests(callback) {
   // Get latest 500 pull requests
-  let cmd = 'git --no-pager log '
-    + '--grep \'Merged in .* (pull request #.*)\' ' // Butbucket format
-    + '--grep \'Merge pull request #.*\' ' // Github format
-    + '--grep \'Merged PR .*: .*\' ' // VisualStudio Team Services format
-    + '--merges '
-    + '--pretty=\'format:__pr-start__%n%h%n%an%n%s%n%b%n__pr-end__\' '
-    + '-500';
+  let cmd =
+    "git --no-pager log " +
+    "--grep 'Merged in .* (pull request #.*)' " + // Butbucket format
+    "--grep 'Merge pull request #.*' " + // Github format
+    "--grep 'Merged PR .*: .*' " + // VisualStudio Team Services format
+    "--pretty='format:__pr-start__%n%h%n%an%n%s%n%b%n__pr-end__' " +
+    "-500";
+
+  if (!argv["all-commits"]) {
+    cmd += " --merged";
+  }
 
   if (argv.since) {
     cmd += ` --date=iso --since='${argv.since}'`;
@@ -59,23 +75,23 @@ function getPullRequests(callback) {
     const pullRequests = [];
     let group;
 
-    stdout.split('\n').forEach((line) => {
+    stdout.split("\n").forEach((line) => {
       if (!line) {
         // Filter empty lines
         return;
       }
 
-      if (line === '__pr-start__') {
+      if (line === "__pr-start__") {
         group = [];
-      } else if (line === '__pr-end__') {
+      } else if (line === "__pr-end__") {
         const commit = group.shift();
         const reviewer = group.shift();
         const subject = group.shift();
         const approvedBy = /^Approved-by:\s([^<]+)\s<([^>]+)>$/;
         let message = group
-          .filter(l => !approvedBy.test(l))
+          .filter((l) => !approvedBy.test(l))
           .join()
-          .replace(/\n|\r/g, '')
+          .replace(/\n|\r/g, "")
           .trim();
 
         // Visual Studio Team Services do not guarantee to provide the PR title within the body
@@ -87,7 +103,7 @@ function getPullRequests(callback) {
         pullRequests.push({
           commit,
           reviewer,
-          message
+          message,
         });
       } else {
         group.push(line);
@@ -103,22 +119,25 @@ function findCommitsInChangelog(callback) {
   const commits = [];
   const input = fs.createReadStream(changelogPath);
   const lineReader = readline.createInterface({
-    input
+    input,
   });
 
-  input.on('error', () => {
-    console.error('Warning: %s does not exist, yet. Attempt to create file now.', changelogName);
+  input.on("error", () => {
+    console.error(
+      "Warning: %s does not exist, yet. Attempt to create file now.",
+      changelogName,
+    );
     callback([]);
   });
 
-  lineReader.on('line', (line) => {
+  lineReader.on("line", (line) => {
     const matches = matcher.exec(line);
     if (matches) {
       commits.push(matches[1]);
     }
   });
 
-  lineReader.on('close', () => {
+  lineReader.on("close", () => {
     callback(commits);
   });
 }
@@ -130,7 +149,7 @@ function matchAbbreviatedCommit(abbrCommit, commits) {
     }
   }
 
-  return commits.some(commit => commit.startsWith(abbrCommit));
+  return commits.some((commit) => commit.startsWith(abbrCommit));
 }
 
 function filterPullRequests(pullRequests, commits) {
@@ -163,10 +182,13 @@ function resolveImplementers(pullRequests, callback) {
       const pullRequest = pullRequests[pos];
 
       // eslint-disable-next-line no-param-reassign
-      pullRequest.implementer = stdout.replace(/\n|\r/g, '').trim();
+      pullRequest.implementer = stdout.replace(/\n|\r/g, "").trim();
 
       // In Bitbucket and Visual Studio Team Services it is not possible to find the reviewer within git data
-      if (argv['hide-reviewer'] || pullRequest.implementer === pullRequest.reviewer) {
+      if (
+        argv["hide-reviewer"] ||
+        pullRequest.implementer === pullRequest.reviewer
+      ) {
         pullRequest.reviewer = null;
       }
 
@@ -178,49 +200,53 @@ function resolveImplementers(pullRequests, callback) {
   next(0);
 }
 
-function stringifyPullRequest({
-  message,
-  implementer,
-  reviewer,
-  commit
-}) {
-  const commitString = argv['link-commit'] ? `[${commit}](${argv['link-commit'].replace(':commit', commit)})` : commit;
+function stringifyPullRequest({ message, implementer, reviewer, commit }) {
+  const commitString = argv["link-commit"]
+    ? `[${commit}](${argv["link-commit"].replace(":commit", commit)})`
+    : commit;
 
-  // Bitbucket case
-  if (!reviewer) {
-    return `- ${message} (i: ${implementer}, c: ${commitString})\n`;
+  const addInfo = [];
+  if (!argv["hide-implementer"]) {
+    addInfo.push(`i: ${implementer}`);
   }
-  return `- ${message} (i: ${implementer}, r: ${reviewer}, c: ${commitString})\n`;
+  if (!argv["hide-reviewer"] && reviewer) {
+    addInfo.push(`r: ${reviewer}`);
+  }
+  addInfo.push(`c: ${commitString}`);
+
+  return `- ${message} (${addInfo.join(", ")})\n`;
 }
 
 function filterPullRequestsInteractively(pullRequests) {
-  return pullRequests.reduce((promise, pullRequest) => (
-    promise.then(async (filteredPullRequests) => {
-      const response = await prompts({
-        type: 'confirm',
-        name: 'value',
-        initial: true,
-        message: stringifyPullRequest(pullRequest)
-      });
+  return pullRequests.reduce(
+    (promise, pullRequest) =>
+      promise.then(async (filteredPullRequests) => {
+        const response = await prompts({
+          type: "confirm",
+          name: "value",
+          initial: true,
+          message: stringifyPullRequest(pullRequest),
+        });
 
-      if (!response.value) {
-        return filteredPullRequests;
-      }
+        if (!response.value) {
+          return filteredPullRequests;
+        }
 
-      return [...filteredPullRequests, pullRequest];
-    })
-  ), Promise.resolve([]));
+        return [...filteredPullRequests, pullRequest];
+      }),
+    Promise.resolve([]),
+  );
 }
 
 function prependToChangelog(str) {
-  let data = Buffer.from('');
+  let data = Buffer.from("");
   try {
     data = fs.readFileSync(changelogPath); // Read existing contents into data
   } catch (error) {
     // Ignore when changelog does not exist yet.
   }
 
-  const fd = fs.openSync(changelogPath, 'w+');
+  const fd = fs.openSync(changelogPath, "w+");
   const buffer = Buffer.from(str);
   fs.writeSync(fd, buffer, 0, buffer.length); // Write new data
   fs.writeSync(fd, data, 0, data.length); // Append old data
@@ -231,21 +257,32 @@ getPullRequests((pullRequests) => {
   findCommitsInChangelog((commits) => {
     const newPullRequests = filterPullRequests(pullRequests, commits);
 
-    resolveImplementers(newPullRequests, () => {
-      (argv.interactive ? filterPullRequestsInteractively(newPullRequests) : Promise.resolve(newPullRequests)).then((finalPullRequests) => {
+    const processPullRequest = () => {
+      (argv.interactive
+        ? filterPullRequestsInteractively(newPullRequests)
+        : Promise.resolve(newPullRequests)
+      ).then((finalPullRequests) => {
         if (finalPullRequests.length === 0) {
-          console.error('Error: There are no new changes!');
+          console.error("Error: There are no new changes!");
           process.exit(1);
         }
 
-        const changeString = finalPullRequests.map(stringifyPullRequest).join('');
+        const changeString = finalPullRequests
+          .map(stringifyPullRequest)
+          .join("");
 
-        if (argv['dry-run']) {
+        if (argv["dry-run"]) {
           console.log(changeString);
         } else {
           prependToChangelog(changeString);
         }
       });
-    });
+    };
+
+    if (argv["hide-implementer"]) {
+      processPullRequest();
+    } else {
+      resolveImplementers(newPullRequests, processPullRequest);
+    }
   });
 });
